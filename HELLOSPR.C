488d8e452b70db269476b4d7133d0fa737ea4bad
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2020-02-01 23:09:39 -0800

    Add files via upload

---
+++ HELLOSPR.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,c,d;
+
+	clrscr(); //clear screen
+
+	printf("Hello World...!!");
+
+       /*	printf("Enter value for a:");
+	scanf("%d", &a);
+
+	printf("Enter value for b:");
+	scanf("%d", &b);
+
+	c = a+b;
+
+	printf("Addition is: %d", c);
+
+	printf("");
+	//scanf("%d", &d);  */
+
+	//getch(); //get character
+}
